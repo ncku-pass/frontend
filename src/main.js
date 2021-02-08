8f4fd2da2fb73abfd571e1e69978b2844316c341
YanTingLin <H34066131@gs.ncku.edu.tw>
2021-02-08 23:16:16 +0800

    refactor: 調整scss引入順序

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
+import '@/scss/index.scss'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/scss/index.scss'
 
 createApp(App).use(store).use(router).mount('#app')
