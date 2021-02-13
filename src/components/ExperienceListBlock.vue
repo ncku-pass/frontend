7c720f9ec482e8e71dd648f10d80bde53979471f
YanTingLin <H34066131@gs.ncku.edu.tw>
2021-02-13 21:24:57 +0800

    refactor: 把學習歷程區塊獨立出來

---
+++ src/components/ExperienceListBlock.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="experience-list-block">
+    <h3 class="experience-list-block__semester">{{props.semester}}</h3>
+    <ul class="experience-list-block__list">
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ExperienceListBlock',
+  props: {
+    semester: String
+  },
+  setup (props) {
+    return { props }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/variables";
+
+.experience-list-block {
+  margin-bottom: 15px;
+  &__semester {
+    margin: 5px;
+    font-size: 26px;
+    font-weight: $weight-medium;
+    color: $gray-3;
+  }
+}
+</style>
--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -1,33 +1,30 @@
 <template>
   <div class="experience">
     <div class="experience__window">
-      <ul class="tabs">
+      <ul class="experience__window__tabs">
         <li>
-          <router-link class="tabs__tab" :to="{name: 'Experience', params: { type: 'class' }}">課程紀錄</router-link>
+          <router-link class="tab" :to="{name: 'Experience', params: { type: 'class' }}">課程紀錄</router-link>
         </li>
         <li>
-          <router-link class="tabs__tab" :to="{name: 'Experience', params: { type: 'activity' }}">活動經驗</router-link>
+          <router-link class="tab" :to="{name: 'Experience', params: { type: 'activity' }}">活動經驗</router-link>
         </li>
         <li>
-          <router-link class="tabs__tab" :to="{name: 'Experience', params: { type: 'competition' }}">競賽經驗</router-link>
+          <router-link class="tab" :to="{name: 'Experience', params: { type: 'competition' }}">競賽經驗</router-link>
         </li>
         <li>
-          <router-link class="tabs__tab" :to="{name: 'Experience', params: { type: 'work' }}">工作經驗</router-link>
+          <router-link class="tab" :to="{name: 'Experience', params: { type: 'work' }}">工作經驗</router-link>
         </li>
         <li>
-          <router-link class="tabs__tab" :to="{name: 'Experience', params: { type: 'certificate' }}">外語能力或證照</router-link>
+          <router-link class="tab" :to="{name: 'Experience', params: { type: 'certificate' }}">外語能力或證照</router-link>
         </li>
         <li>
-          <router-link class="tabs__tab" :to="{name: 'Experience', params: { type: 'others' }}">其他</router-link>
+          <router-link class="tab" :to="{name: 'Experience', params: { type: 'others' }}">其他</router-link>
         </li>
       </ul>
-      <div class="experiences-table">
-        <div class="experiences-table__block">
-          <h3 class="experiences-table__block__semester">106-1</h3>
-          <ul class="experiences-table__block__list">
-            <ExperienceListItem />
-          </ul>
-        </div>
+      <div class="experience__window__table">
+        <ExperienceListBlock v-for="semesterData in allData" :key="semesterData.semester" :semester="semesterData.semester">
+          <ExperienceListItem v-for="experience in semesterData.experiences" :key="experience.name" v-bind="experience"/>
+        </ExperienceListBlock>
       </div>
     </div>
   </div>
@@ -35,11 +32,43 @@
 
 <script>
 import ExperienceListItem from '@/components/ExperienceListItem.vue'
+import ExperienceListBlock from '@/components/ExperienceListBlock.vue'
+
+const allData = [
+  {
+    semester: '106-1',
+    experiences: [
+      { name: '系統分析與設計', tags: ['國際視野', '數理能力'] },
+      { name: '互動介面設計松', tags: ['設計能力', '介面設計'] },
+      { name: '大數據分析與資料探勘', tags: ['python', '數據分析'] }
+    ]
+  },
+  {
+    semester: '106-2',
+    experiences: [
+      { name: '系統分析與設計', tags: ['國際視野', '數理能力'] },
+      { name: '互動介面設計松', tags: ['設計能力', '介面設計'] },
+      { name: '大數據分析與資料探勘', tags: ['python', '數據分析'] }
+    ]
+  },
+  {
+    semester: '107-1',
+    experiences: [
+      { name: '系統分析與設計', tags: ['國際視野', '數理能力'] },
+      { name: '互動介面設計松', tags: ['設計能力', '介面設計'] },
+      { name: '大數據分析與資料探勘', tags: ['python', '數據分析'] }
+    ]
+  }
+]
 
 export default {
   name: 'Experience',
   components: {
-    ExperienceListItem
+    ExperienceListItem,
+    ExperienceListBlock
+  },
+  setup () {
+    return { allData }
   }
 }
 </script>
@@ -62,26 +91,22 @@ export default {
   box-shadow: 0px 0px 30px rgba(241, 90, 96, 0.05), 0px 0px 25px rgba(241, 90, 96, 0.1);
 }
 
-.tabs {
+.experience__window__tabs {
   @include grid(column, 0, 20px);
   padding: 8px 10px;
   list-style: none;
   box-shadow: 0px 4px 15px rgba(241, 90, 96, 0.06);
-  &__tab {
-    @include tab-link;
-  }
 }
 
-.experiences-table {
-  padding: 24px 80px;
+.tab {
+  @include tab-link;
 }
 
-.experiences-table__block {
-  &__semester {
-    margin: 5px;
-    font-size: 26px;
-    font-weight: $weight-medium;
-    color: $gray-3;
+.experience__window__table {
+  padding: 24px 80px 0;
+  overflow-y: auto;
+  &::-webkit-scrollbar {
+    display: none;
   }
 }
 </style>
