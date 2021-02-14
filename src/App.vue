717567e92d74e86ebc07d512e4ef3780bdcc48f3
YanTingLin <H34066131@gs.ncku.edu.tw>
2021-02-15 02:22:26 +0800

    feat: 更新主畫面背景

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,9 @@ export default {
   flex-direction: column;
   height: 100vh;
   background: rgb(255,255,255);
-  background: linear-gradient(90deg, white 300px, $pink 300px);
+  background:
+    url('./assets/bg_dot.svg') no-repeat,
+    linear-gradient(90deg, white 300px, $pink 300px);
 }
 
 main {
