57b59426a17f18aa3f03b458e9b86a68844d8536
YanTingLin <H34066131@gs.ncku.edu.tw>
2021-02-15 19:18:46 +0800

    feat: 新增兩款modal

---
+++ src/components/ConfirmModal.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="confirm-modal" @click.self="$emit('cancel')">
+    <div class="confirm-modal__content">
+      {{ message }}
+      <div class="confirm-modal__btns">
+        <button class="btn" @click="$emit('cancel')">
+          取消
+        </button>
+        <button class="btn--danger" @click="$emit('confirm')">
+          {{ confirmMessage }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConfirmModal',
+  props: {
+    message: {
+      type: String,
+      default: '訊息'
+    },
+    confirmMessage: {
+      type: String,
+      default: '確定刪除'
+    }
+  },
+  emits: ['cancel', 'confirm'],
+  setup (props, context) {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss">
+.confirm-modal {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: grid;
+  place-items: center;
+  background-color: rgba(0, 0, 0, 0.25);
+  &__content {
+    padding: 10px 24px;
+    width: 230px;
+    line-height: 24px;
+    text-align: center;
+    background-color: #fff;
+    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+  }
+  &__btns {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+  }
+}
+</style>
---
+++ src/components/MessageModal.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    class="message-modal"
+    @click.self="$emit('close')"
+  >
+    <div class="message-modal__content">
+      {{ message }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MessageModal',
+  props: {
+    message: {
+      type: String,
+      default: '訊息'
+    },
+    duration: {
+      type: Number,
+      default: 1000
+    }
+  },
+  emits: ['close'],
+  setup (props, context) {
+    setTimeout(() => {
+      context.emit('close')
+    }, props.duration)
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss">
+.message-modal {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: grid;
+  place-items: center;
+  background-color: rgba(0, 0, 0, 0.25);
+  &__content {
+    padding: 11px 12px;
+    width: 230px;
+    line-height: 24px;
+    text-align: center;
+    background-color: #fff;
+    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+  }
+}
+</style>
