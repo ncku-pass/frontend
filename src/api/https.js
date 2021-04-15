b96d7ba5ecb79c8afe21b67ef2001dca6f250cf8
YanTingLin <H34066131@gs.ncku.edu.tw>
2021-04-16 03:21:03 +0800

    feat: 更新api位置

--- src/api/https.js
+++ src/api/https.js
@@ -2,9 +2,10 @@ import axios from 'axios'
 
 axios.defaults.timeout = 7000
 // axios.defaults.headers.common.Authorization = `Bearer ${process.env.VUE_APP_AUTHORIZATION_TOKEN}`
-axios.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem('auth')}`
+// axios.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem('auth')}`
 
-const baseURL = 'http://139.162.83.11:5003/api'
+// const baseURL = 'http://139.162.83.11:5003/api'
+const baseURL = 'https://e-portfolio-ncku.herokuapp.com/api'
 
 export const experiencesRequest = axios.create({
   baseURL: `${baseURL}/experiences`
