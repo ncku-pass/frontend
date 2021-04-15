c6bcbec4d4fe12869112c088fbf00b4b31ccbb32
TomatoGuy0502 <H34066131@gs.ncku.edu.tw>
2021-04-15 17:23:53 +0800

    feat: 新增landing介紹頁面與footer

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -119,27 +119,50 @@
         </div>
       </div>
     </div>
-    <div class="section introduction">
-      <div class="container">
-        <img class="introduction__img">
+    <div class="section">
+      <div class="container introduction">
+        <img class="introduction__img" src="@/assets/landing-1.png">
         <div class="introduction__text">
-          個人化紀錄
+          <h2>個人化紀錄</h2>
+          <p>內建校內學習紀錄，以簡潔快速的方式一覽大學學習歷程，可以即時的在已修習過的課程進行紀錄，更能透過自行增減不同的學習紀錄，來增添你精彩大學的學習經歷。</p>
         </div>
       </div>
     </div>
-    <div class="section introduction">
-      <div class="container">
-        <img class="introduction__img">
+    <div class="section">
+      <div class="container introduction introduction--reverse">
+        <img class="introduction__img" src="@/assets/landing-2.png">
         <div class="introduction__text">
-          歸納出關鍵能力
+          <h2>歸納出關鍵能力</h2>
+          <p>提供個人專屬的分類工具，以TAG 來標示課程所得到的能力，透過分類 TAG 歸納出個人的關鍵技能，快速歸納自身特質及專長，精準統整，侃侃而談個人能力不再是難事。</p>
         </div>
       </div>
     </div>
-    <div class="section introduction">
-      <div class="container">
-        <img class="introduction__img">
+    <div class="section">
+      <div class="container introduction">
+        <img class="introduction__img" src="@/assets/landing-3.png">
         <div class="introduction__text">
-          展現個人優勢
+          <h2>展現個人優勢</h2>
+          <p>可以針對履歷、研究所備審等編排不同版本，在各版本中能主題式的展現個人能力，不再擔心不會排版，只需拉入各項經驗即可，最後一鍵即能輸出簡潔的個人履歷。</p>
+        </div>
+      </div>
+    </div>
+    // TODO: 等待設計師修正
+    <div class="section section-footer">
+      <div class="section-footer__bg" />
+      <div class="container section-footer__content">
+        <div class="section-footer__summary">
+          E-portfolio幫助學生<span class="red">有效的</span>整理經歷，並<span class="red">全方位</span>展現個人特質
+        </div>
+        <div class="section-footer__btns">
+          <button class="btn">
+            馬上登入
+          </button>
+          <button class="btn--danger">
+            立即註冊
+          </button>
+        </div>
+        <div class="section-footer__footer">
+          <p>© NCKU 教學發展中心版權所有</p>
         </div>
       </div>
     </div>
@@ -161,7 +184,8 @@ export default {
       if (!isInit.value) {
         fullpage('.landing', {
           licenseKey: "i#eq#uS42Kr]C~j4g/m~3P;2R+( &83iM?yko4P'HZ9E|k1-x:<okp",
-          easingcss3: 'cubic-bezier(0.64, 0.055, 0.345, 1)'
+          easingcss3: 'cubic-bezier(0.64, 0.055, 0.345, 1)',
+          anchors: ['1', '2', '3', '4', '5', '6']
         })
         isInit.value = true
       }
@@ -283,6 +307,83 @@ export default {
   }
   &__desc {
     color: $gray-3;
+  }
+}
+.introduction {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  &--reverse {
+    flex-direction: row-reverse;
+  }
+  &__img {
+    width: 445px;
+  }
+  &__text {
+    width: 540px;
+    h2 {
+      color: $red-light;
+      font-weight: $weight-black;
+      margin-top: 0;
+      margin-bottom: 50px;
+      font-size: 60px;
+    }
+    p {
+      font-size: 21px;
+      text-indent : 2em;
+      line-height: 34px;
+    }
+  }
+}
+.section-footer {
+  &__content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  &__bg {
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    width: 100%;
+    height: 480px;
+    background: url('~@/assets/chatting.png') no-repeat center;
+    background-size: cover;
+  }
+  &__summary {
+    padding: 8px 20px;
+    margin-bottom: 50px;
+    background: rgba(255, 255, 255, 0.69);
+    border-radius: 10px;
+    color: $gray-1;
+    text-align: center;
+    font-weight: 900;
+    font-size: 60px;
+    line-height: 97px;
+    .red {
+      color: $red-light;
+    }
+  }
+  &__btns {
+    @include grid(column, 0, 60px);
+    button {
+      font-size: 32px;
+      line-height: 52px;
+    }
+  }
+  &__footer {
+    z-index: -1;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    position: absolute;
+    bottom: 0;
+    padding: 27px;
+    height: 200px;
+    width: 100%;
+    background-color: $red;
+    text-align: center;
+    color: $white;
   }
 }
 
