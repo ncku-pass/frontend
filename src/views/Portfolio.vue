98057743893eba8f2325b1ef2bb79cbc0c6dcf2f
TomatoGuy0502 <H34066131@gs.ncku.edu.tw>
2021-08-04 02:00:05 +0800

    feat: 新增portfolio loading畫面、修改顏色變數

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="portfolio">
-    <PortfolioMain />
-    <PortfolioMenu />
+    <template v-if="loading">
+      <div class="portfolio__loading">
+        <p>Loading</p>
+        <div class="lds-dual-ring" />
+      </div>
+      <div class="portfolio__loading-left" />
+      <div class="portfolio__loading-right" />
+    </template>
+    <template v-else>
+      <PortfolioMain />
+      <PortfolioMenu />
+    </template>
   </div>
 </template>
 
 <script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
 import PortfolioMain from '@/components/Portfolio/PortfolioMain'
 import PortfolioMenu from '@/components/Portfolio/PortfolioMenu'
 
@@ -16,17 +28,56 @@ export default {
     PortfolioMenu
   },
   setup () {
+    const store = useStore()
+
+    const resumesNotReady = computed(() => store.state.resumes.isPending)
+    const tagsNotReady = computed(() => store.state.tags.isPending)
+    const experiencesNotReady = computed(() => store.state.experiences.isPending)
+
+    const loading = computed(() => resumesNotReady.value || tagsNotReady.value || experiencesNotReady.value)
+
+    return {
+      loading
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '~@/scss/variables';
+
 .portfolio {
+  position: relative;
   display: grid;
   grid-template-columns: 7fr 3fr;
   gap: 0 30px;
   height: 100%;
   max-width: 1110px;
   margin: 0 auto;
+  &__loading {
+    position: absolute;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    font-size: 26px;
+  }
+}
+.portfolio__loading-left {
+  margin: 26px 0;
+  background-color: $gray-6;
+  filter: drop-shadow(-2px 4px 30px rgba(241, 90, 96, 0.05)) drop-shadow(-2px 4px 30px rgba(241, 90, 96, 0.1));
+  border-radius: 10px 10px 0px 0px;
+  height: 70%;
+}
+.portfolio__loading-right {
+  margin: 26px 0;
+  background-color: $gray-6;
+  border-radius: 10px;
 }
 </style>
