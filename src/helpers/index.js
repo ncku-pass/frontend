299cf8182806c658cf0faadd64e9bfe08344a293
TomatoGuy0502 <H34066131@gs.ncku.edu.tw>
2021-08-11 01:20:59 +0800

    fix: 修正學年學期與日期轉換

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -51,10 +51,10 @@ export const debounce = (func, delay = 250) => {
 export const semesterToDate = (semester = '110-2') => {
   const [year, term] = semester.split('-')
   if (term === '1') {
-    const dateString = `${~~year + 1910}-09-01`
+    const dateString = `${~~year + 1911}-09-01`
     return new Date(dateString)
   } else {
-    const dateString = `${~~year + 1911}-02-01`
+    const dateString = `${~~year + 1912}-02-01`
     return new Date(dateString)
   }
 }
@@ -63,10 +63,10 @@ export const dateToSemester = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
   if (month >= 9) {
-    return `${year - 1910}-1`
-  } else if (month >= 2) {
-    return `${year - 1911}-2`
-  } else {
     return `${year - 1911}-1`
+  } else if (month >= 2) {
+    return `${year - 1912}-2`
+  } else {
+    return `${year - 1912}-1`
   }
 }
