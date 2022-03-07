3c63a72a95f6ac1183067e884975164f50cc6034
Fung Hei Man <94099670+fung-hei-man@users.noreply.github.com>
2022-03-07 20:53:17 +0800

    Revert "Merge branch 'master' into develop"
    This reverts commit fef6667ca6735b3bb4384dfe580f5c9b5a253568, reversing
    changes made to 6c4b7331e40172f27b8616b631d17b34e84cc0b2.

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,75 +1,75 @@
 <template>
-  <div class="landing">
-    <section class="hero">
-      <div class="container hero__content">
-        <div class="intro">
-          <img class="intro__icon" src="@/assets/brand_icon_white.svg" alt="brand__icon" />
-          <h1 class="intro__title">
+  <div class='landing'>
+    <section class='hero'>
+      <div class='container hero__content'>
+        <div class='intro'>
+          <img class='intro__icon' src='@/assets/brand_icon_white.svg' alt='brand__icon' />
+          <h1 class='intro__title'>
             幫助你整理經歷，<br />
             全方位展現個人特質
           </h1>
-          <h2 class="intro__subtitle">
-            NCKU Experience 陪你整理學習經歷
+          <h2 class='intro__subtitle'>
+            NCKU PASS 陪你整理學習經歷
           </h2>
         </div>
-        <div class="login">
-          <h2 class="login__title">
-            登入 NCKU Experience
+        <div class='login'>
+          <h2 class='login__title'>
+            登入 NCKU PASS
           </h2>
-          <a href="https://i.ncku.edu.tw/ncku/oauth/eportfolio/login.php" class="ncku-login">
-            使用<img src="@/assets/ncku_login.png" alt="ncku login" />登入
+          <a href='https://i.ncku.edu.tw/ncku/oauth/eportfolio/login.php' class='ncku-login'>
+            使用<img src='@/assets/ncku_login.png' alt='ncku login' />登入
           </a>
         </div>
       </div>
       <svg
-        class="scroll-arrow"
-        width="44"
-        height="29"
-        viewBox="0 0 44 29"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
+        class='scroll-arrow'
+        width='44'
+        height='29'
+        viewBox='0 0 44 29'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
       >
-        <path d="M3 2L22 24L41 2" stroke="white" stroke-width="5" />
+        <path d='M3 2L22 24L41 2' stroke='white' stroke-width='5' />
       </svg>
     </section>
-    <div class="container">
-      <section class="features">
-        <h2 class="features__title">
-          NCKU Experience 三大功能
+    <div class='container'>
+      <section class='features'>
+        <h2 class='features__title'>
+          NCKU PASS 三大功能
         </h2>
-        <ul class="features__list">
-          <li class="features__item">
-            <img src="@/assets/card-record.svg" alt="" class="features__item__img" />
-            <h3 class="features__item__title">
+        <ul class='features__list'>
+          <li class='features__item'>
+            <img src='@/assets/card-record.svg' alt='' class='features__item__img' />
+            <h3 class='features__item__title'>
               紀錄大學各項經驗
             </h3>
           </li>
-          <li class="features__item">
-            <img src="@/assets/card-plan.svg" alt="" class="features__item__img" />
-            <h3 class="features__item__title">
+          <li class='features__item'>
+            <img src='@/assets/card-plan.svg' alt='' class='features__item__img' />
+            <h3 class='features__item__title'>
               以TAG 整理個人能力
             </h3>
           </li>
-          <li class="features__item">
-            <img src="@/assets/card-portfolio.svg" alt="" class="features__item__img" />
-            <h3 class="features__item__title">
+          <li class='features__item'>
+            <img src='@/assets/card-portfolio.svg' alt='' class='features__item__img' />
+            <h3 class='features__item__title'>
               簡易製作履歷
             </h3>
           </li>
         </ul>
       </section>
-      <section class="feature">
-        <img class="feature__img" src="@/assets/landing-1.png" />
-        <div class="feature__text">
+      <section class='feature'>
+        <img class='feature__img' src='@/assets/landing-1.png' />
+        <div class='feature__text'>
           <h2>個人化紀錄</h2>
           <p>
             內建校內學習紀錄，以簡潔快速的方式一覽大學學習歷程，可以即時的在已修習過的課程進行紀錄，更能透過自行增減不同的學習紀錄，來增添你精彩大學的學習經歷。
           </p>
         </div>
       </section>
-      <section class="feature feature--reverse">
-        <img class="feature__img" src="@/assets/landing-2.png" />
-        <div class="feature__text">
+      <section class='feature feature--reverse'>
+        <img class='feature__img' src='@/assets/landing-2.png' />
+        <div class='feature__text'>
           <h2>以TAG整理個人能力</h2>
           <p>
             提供個人專屬的分類工具，以TAG 來標示課程所得到的能力，透過分類 TAG
@@ -77,9 +77,9 @@
           </p>
         </div>
       </section>
-      <section class="feature">
-        <img class="feature__img" src="@/assets/landing-3.png" />
-        <div class="feature__text">
+      <section class='feature'>
+        <img class='feature__img' src='@/assets/landing-3.png' />
+        <div class='feature__text'>
           <h2>簡易製作履歷</h2>
           <p>
             可以針對履歷、研究所備審等編排不同版本，在各版本中能主題式的展現個人能力，不再擔心不會排版，只需拉入各項經驗即可，最後一鍵即能輸出簡潔的個人履歷。
@@ -87,19 +87,19 @@
         </div>
       </section>
     </div>
-    <footer class="footer">
-      <div class="container">
-        <img class="footer__icon" src="@/assets/brand_icon_white.svg" alt="brand icon" />
-        <p class="footer__info">
+    <footer class='footer'>
+      <div class='container'>
+        <img class='footer__icon' src='@/assets/brand_icon_white.svg' alt='brand icon' />
+        <p class='footer__info'>
           © NCKU 教學發展中心版權所有<br /><br />
           701 台南市大學路一號光復校區雲平大樓3樓<br />
-          Mail：<a href="mailto:nckuexperience@gmail.com">nckuexperience@gmail.com</a><br /><br />
+          Mail：<a href='mailto:nckuexperience@gmail.com'>nckuexperience@gmail.com</a><br /><br />
           [ 若有任何問題歡迎寄信與我們聯繫或填寫以下表單 ]<br />
-          <a href="https://forms.gle/tSSNHvQERSyVsHMJ6" target="_blank">回饋表單</a>
+          <a href='https://forms.gle/tSSNHvQERSyVsHMJ6' target='_blank'>回饋表單</a>
         </p>
-        <div class="footer__links">
-          <a href="https://web.ncku.edu.tw/" target="_blank">國立成功大學 National Cheng Kung University</a>
-          <a href="https://ctld-acad.ncku.edu.tw/" target="_blank">教學發展中心</a>
+        <div class='footer__links'>
+          <a href='https://web.ncku.edu.tw/' target='_blank'>國立成功大學 National Cheng Kung University</a>
+          <a href='https://ctld-acad.ncku.edu.tw/' target='_blank'>教學發展中心</a>
         </div>
       </div>
     </footer>
@@ -108,8 +108,8 @@
 
 <script>
 export default {
-  name: 'Landing'
-}
+  name: 'Landing',
+};
 </script>
 
 <style lang="scss" scoped>
