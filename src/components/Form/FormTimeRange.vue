86a9b20674479e7a276517ca777dbdc21c8bf2d6
fung-hei-man <94099670+fung-hei-man@users.noreply.github.com>
2022-10-05 17:28:58 +0800

    fix: time zone issue for datepicker

--- src/components/Form/FormTimeRange.vue
+++ src/components/Form/FormTimeRange.vue
@@ -39,13 +39,16 @@ export default {
       : props.inputKey.map((key) => props.initValue[key] === null ? null : new Date(props.initValue[key])))
 
     const onDateSelect = (val) => {
+      // primevue gives date in format Tue Jun 15 2021 00:00:00 GMT+0800 (Taipei Standard Time)
+      // find the index before converting
       const idx = value.value.findIndex((item) => item === val)
+      const convertedDate = new Date(Date.UTC(val.getFullYear(), val.getMonth(), val.getDate())).toISOString()
 
       // start date, clear end date
       if (idx === 0) {
         emit('input', { subKey: props.inputKey[idx + 1], value: null })
       }
-      emit('input', { subKey: props.inputKey[idx], value: val })
+      emit('input', { subKey: props.inputKey[idx], value: convertedDate })
     }
 
     return {
